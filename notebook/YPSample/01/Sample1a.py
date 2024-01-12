0a524ec0f708ad021f84f3a38161d4e84dc90295
ARAI Tomoya <ahiru3touhei@gmail.com>
2024-01-12 10:06:08 +0900

    aaa

---
+++ notebook/YPSample/01/Sample1a.py
@@ -0,0 +1,1 @@
+print("ようこそPythonへa!")
---
+++ notebook/YPSample/Lesson2/test.py
@@ -0,0 +1,25 @@
+###Sample1
+#画面に出力する
+print("ようこそPythonへ!")
+print("Pythonをはじめましょう!")
+
+"ようこそ\
+Pythonへ!"
+
+'Hello'
+"こんにちは"
+"ようこそPythonへ"
+
+"""ようこそ
+Pythonへ!"""
+
+###Sample2
+print(1)
+print(3.14)
+
+#### Sample3
+print("10進数の10は",10,"です。")
+print("2進数の10は",0b10,"です。")
+print("8進数の10は",0o10,"です。")
+print("16進数の10は",0x10,"です。")
+print("10進数のFは",0xF,"です。")
