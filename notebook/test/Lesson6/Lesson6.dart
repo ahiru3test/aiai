e575b7ef10338132a4e0a630142d379fae42ae55
ahiru3to <ahiru3touhei@gmail.com>
2024-01-22 10:04:26 +0900

    aaa

--- notebook/test/Lesson6/Lesson6.dart
+++ notebook/test/Lesson6/Lesson6.dart
@@ -82,5 +82,68 @@ void main(){
   print("");
 
 
+  print("\nSample5");
+  var sale1={"東京":80, "名古屋":60,"京都":22};
+  var sale2={"京都":100,"大阪":50,"福岡":75};
+  print("現在のデータは"+sale1.toString()+"です。");
+  print("現在のデータは"+sale2.toString()+"です。");
+
+  print("1を2で更新します。");
+
+  sale1.addAll(sale2);
+
+  print("1のデータは"+sale1.toString()+"です。");
+
+
+  print("\nSample6");
+  var city={"東京", "名古屋","京都","大阪","福岡"};
+  print("現在のデータは"+city.toString()+"です。");
+
+  // var d=input("追加するデータを入力してください。");
+  var d="横浜";
+  if (city.contains(d)) {
+    print(d+"のデータはすでに存在しています。");
+  } else {
+    city.add(d);
+    print(d+"を追加しました。");
+  }
+  print("現在のデータは"+city.toString()+"です。");
+
+  d="京都";
+  if (city.contains(d)) {
+    city.remove(d);
+    print(d+"を削除しました。");
+  } else {
+    print(d+"のデータはみつかりませんでした。");
+  }
+  print("現在のデータは"+city.toString()+"です。");
+
+  print("\nSample7");
+  var cityA={"東京","名古屋","京都","大阪"};
+  var cityB={"京都","大阪","福岡"};
+
+  print("Aの都市名は"+cityA.toString()+"です。");
+  print("Bの都市名は"+cityB.toString()+"です。");
+
+  print("共通するデータは"+(cityA.intersection(cityB)).toString()+"です。");
+  print("Aのみのデータは"+(cityA.difference(cityB)).toString()+"です。");
+  print("Bのみのデータは"+(cityB.difference(cityA)).toString()+"です。");
+  print("すべてのデータは"+(cityA.union(cityB)).toString()+"です。");
+
+
+  print("\nRensyuu");
+  var data={};
+  data["a1"]=10;
+  print(data);
+  data.remove("a1");
+  print(data);
+
+  var data2=Set();
+  data2.add("a1");
+  print(data2);
+  data2.remove("a1");
+  print(data2);
+
+  print(data.runtimeType.toString()+"\t"+data2.runtimeType.toString());
 
 }
---
+++ notebook/test/Lesson7/Lesson7.dart
@@ -0,0 +1,5 @@
+import 'dart:core';
+// import 'dart:io';
+
+void main(){
+}
