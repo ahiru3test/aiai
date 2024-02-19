50db5faa91c947ffc07dee6d90d6105259c5c03f
ahiru3to <ahiru3touhei@gmail.com>
2024-02-19 18:03:56 +0900

    aaa

---
+++ notebook/g_test/mvc/model.py
@@ -0,0 +1,16 @@
+class Model:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def move_up(self):
+        self.y -= 1
+
+    def move_down(self):
+        self.y += 1
+
+    def move_left(self):
+        self.x -= 1
+
+    def move_right(self):
+        self.x += 1
